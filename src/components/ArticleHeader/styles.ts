48c35ae6f5dfd29c780d081b6d9fe300f47c7794
Evandro Calado <evandrocalado07@gmail.com>
2023-04-08 15:08:56 -0300

    fix img Article Header

--- src/components/ArticleHeader/styles.ts
+++ src/components/ArticleHeader/styles.ts
@@ -32,6 +32,8 @@ export const Excerpt = styled.p`
 export const Cover = styled.img`
   ${({ theme }) => css`
     width: 100%;
+    height: 60.3rem;
+    object-fit: cover;
     display: block;
     margin-bottom: ${theme.spacings.medium};
   `}
